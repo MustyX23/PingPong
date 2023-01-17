48cbcea19a0dd30aee27d12999f5d2cfec52c121
MustyX23 <mustyx900@gmail.com>
2023-01-17 22:32:26 +0200

    -First Upload - SETTING THE FIELD UP-

--- PingPong/Program.cs
+++ PingPong/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PingPong
 {
@@ -6,7 +7,37 @@ namespace PingPong
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Setting the Field UP...
+
+            const int fieldLength = 50;
+            const int fieldWidth = 15;
+            const char fieldTile = '#';
+
+            string line = string.Concat(Enumerable.Repeat(fieldTile, fieldLength));
+
+            const int racketLength = fieldWidth / 4;
+            const char racketTile = '|';
+
+            int rightRacketHeight = 0;
+            int leftRacketHeight = 0;
+
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(line);
+
+                Console.SetCursorPosition(0, fieldWidth);
+                Console.WriteLine(line);
+
+                for (int i = 0; i < racketLength; i++)
+                {
+                    Console.SetCursorPosition(0, i + 1 + leftRacketHeight);
+                    Console.WriteLine(racketTile);
+                    Console.SetCursorPosition(fieldLength - 1, i + 1 + rightRacketHeight);
+                    Console.WriteLine(racketTile);
+                }
+            }
+
         }
     }
 }
